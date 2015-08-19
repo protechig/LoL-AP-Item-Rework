8b96fcd724f931a4dc65db3ddce5d2020eb22599
Trevor Maglione <tmaglione@fb.com>
2015-08-19 13:25:38 -0700

    Start helpers and use success in responses

--- ap-split.rb
+++ ap-split.rb
@@ -11,8 +11,26 @@ set :client, Lol::Client.new(settings.apikey,
                                :redis  => 'redis://localhost:6379',
                                :ttl    => 900 })
 
+helpers do
+  def get_match(id)
+    settings.client.match.get id
+  end
+end
+
 get '/match/:id' do |id|
   content_type :json
-  match = settings.client.match
-  match.get(id).to_json
+  begin
+    match = get_match id
+    response = {
+      :success => true,
+      :match   => match
+    }
+  rescue Exception => e
+    response = {
+      :success => false,
+      :error   => e.message
+    }
+  end
+
+  response.to_json
 end
